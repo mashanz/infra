ea02a441ff3bdd681f2e657e4c0292d9f3ace3b9
Hanjara Adhyatma ( Mashanz ) <mangatkk@gmail.com>
2022-12-26 12:35:56 +0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-INFRA_VERSION=stable
-wget https://github.com/mashanz/infra/archive/refs/branchs/${INFRA_VERSION}.tar.gz -O /tmp/infra.tar.gz
+INFRA_VERSION=1.0.1
+wget https://github.com/mashanz/infra/archive/refs/tags/${INFRA_VERSION}.tar.gz -O /tmp/infra.tar.gz
 tar -xf /tmp/infra.tar.gz -C /tmp/infra
 cd /tmp/infra-${INFRA_VERSION}
 bash ./install.sh
